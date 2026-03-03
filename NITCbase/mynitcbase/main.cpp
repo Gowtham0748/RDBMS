12ca1bb35b5e861844aae55990a5692ecd21a7b6
Gowtham0748 <gowthamgsl0748@gmail.com>
2026-03-03 09:09:17 +0530

    Stage 2 Record Blocks and Catalog Structures

--- NITCbase/mynitcbase/main.cpp
+++ NITCbase/mynitcbase/main.cpp
@@ -5,21 +5,127 @@
 #include <cstring>
 #include <iostream>
 
-int main(int argc, char *argv[])
-{
-  /* Initialize the Run Copy of Disk */
-  Disk disk_run;
-  // reading blocks and and printing its values
-  unsigned char buffer[BLOCK_SIZE];
-  Disk::readBlock(buffer, 0);
-  for (int i = 0; i < 10; i++)
-  {
-    std::cout << (int)buffer[i] << " ";
-  }
-  std::cout << "\n";
 
-  return 0;
+//when multiple attribute blocks present, linked as a ll
+
+int main(int argc, char *argv[]) {
+    Disk disk_run;
+
+    RecBuffer relCatBuffer(RELCAT_BLOCK);
+    HeadInfo relCatHeader;
+    relCatBuffer.getHeader(&relCatHeader);
+
+    for (int i = 0; i < relCatHeader.numEntries; i++) {
+
+        Attribute relCatRecord[RELCAT_NO_ATTRS];
+        relCatBuffer.getRecord(relCatRecord, i);
+
+        printf("Relation: %s\n",
+               relCatRecord[RELCAT_REL_NAME_INDEX].sVal);
+
+        // ---- Attribute catalog traversal (MULTI-BLOCK) ----
+        int currBlock = ATTRCAT_BLOCK;
+
+        while (currBlock != -1) {
+
+            RecBuffer attrCatBuffer(currBlock);
+            HeadInfo attrHead;
+            attrCatBuffer.getHeader(&attrHead);
+
+            for (int j = 0; j < attrHead.numEntries; j++) {
+
+                Attribute attrCatRecord[ATTRCAT_NO_ATTRS];
+                attrCatBuffer.getRecord(attrCatRecord, j);
+
+                if (strcmp(attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal,relCatRecord[RELCAT_REL_NAME_INDEX].sVal) == 0) {
+
+                    const char *attrType =
+                        (attrCatRecord[ATTRCAT_ATTR_TYPE_INDEX].nVal == NUMBER)
+                        ? "NUM"
+                        : "STR";
+
+                    printf("  %s: %s\n",attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal,attrType);
+                }
+            }
+
+            // move to next attribute catalog block
+            currBlock = attrHead.rblock;
+        }
+
+        printf("\n");
+    }
+
+    return 0;
 }
+
+
+
+
+
+
+
+
+
+// //printing relational catalog and attribute catalog, attributes and its types
+// int main(int argc, char *argv[]) {
+//   Disk disk_run;
+
+//   // create objects for the relation catalog and attribute catalog
+//   RecBuffer relCatBuffer(RELCAT_BLOCK);
+//   RecBuffer attrCatBuffer(ATTRCAT_BLOCK);
+
+//   HeadInfo relCatHeader;
+//   HeadInfo attrCatHeader;
+
+//   // load the headers of both the blocks into relCatHeader and attrCatHeader.
+//   // (we will implement these functions later)
+//   relCatBuffer.getHeader(&relCatHeader);
+//   attrCatBuffer.getHeader(&attrCatHeader);
+
+//   for (int i=0; i< relCatHeader.numEntries;i++/* i = 0 to total relation count */) {
+
+//     Attribute relCatRecord[RELCAT_NO_ATTRS]; // will store the record from the relation catalog
+
+//     relCatBuffer.getRecord(relCatRecord, i);
+
+//     printf("Relation: %s\n", relCatRecord[RELCAT_REL_NAME_INDEX].sVal);
+
+//     for (int j=0;j<attrCatHeader.numEntries;j++/* j = 0 to number of entries in the attribute catalog */) {
+
+//       // declare attrCatRecord and load the attribute catalog entry into it
+//       Attribute attrCatRecord[ATTRCAT_NO_ATTRS];
+//       attrCatBuffer.getRecord(attrCatRecord,j);
+
+//       if (strcmp(attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal,relCatRecord[RELCAT_REL_NAME_INDEX].sVal)==0/* attribute catalog entry corresponds to the current relation */) {
+//         const char *attrType = attrCatRecord[ATTRCAT_ATTR_TYPE_INDEX].nVal == NUMBER ? "NUM" : "STR";
+//         printf("  %s: %s\n", /* get the attribute name */attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal, attrType);
+//       }
+//     }
+//     printf("\n");
+//   }
+
+//   return 0;
+// }
+
+
+
+
+
+// int main(int argc, char *argv[])
+// {
+//   /* Initialize the Run Copy of Disk */
+//   Disk disk_run;
+//   // reading blocks and and printing its values
+//   unsigned char buffer[BLOCK_SIZE];
+//   Disk::readBlock(buffer, 0);
+//   for (int i = 0; i < 10; i++)
+//   {
+//     std::cout << (int)buffer[i] << " ";
+//   }
+//   std::cout << "\n";
+
+//   return 0;
+// }
 
 /*using this we can rad from a block and we can write back to it*/
 // unsigned char buffer[BLOCK_SIZE];
